bddacd93649e705011585625741f07d019584cfb
Andres Petralli <andres.petralli@microsoft.com>
2021-08-20 11:21:28 -0700

    Simplify

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
